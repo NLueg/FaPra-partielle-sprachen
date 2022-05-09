add0f0edcf49d4fcdfeb836e307f78a1b89346fa
Nico Lueg <nicolueg@yahoo.de>
2022-05-09 05:49:45 +0200

    refactor(upload): use types instead of classes

--- src/app/classes/run/run.ts
+++ src/app/classes/run/run.ts
@@ -1,17 +1,5 @@
 import { Transition } from './transition';
 
-export class Run {
-    private readonly _transitions: Array<Transition>;
-
-    constructor() {
-        this._transitions = [];
-    }
-
-    get transitions(): Array<Transition> {
-        return this._transitions;
-    }
-
-    public addTransition(transition: Transition): void {
-        this._transitions.push(transition);
-    }
-}
+export type Run = {
+    transitions: Array<Transition>;
+};
