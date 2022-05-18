73b8927dbe159277b6ab25f5c4977fa4a4f770ab
elisabeth brandl <elisabeth_brandl@gmx.net>
2022-05-18 15:05:39 +0200

    lint:fix

--- src/app/classes/diagram/run.ts
+++ src/app/classes/diagram/run.ts
@@ -18,37 +18,34 @@ export class Run {
     }
 
     addElement(element: Element): void {
-        let contains:boolean;
-        contains=false;
-        for (const item of this._elements){
-            if (item.label===element.label){
-                contains=true;
+        let contains: boolean;
+        contains = false;
+        for (const item of this._elements) {
+            if (item.label === element.label) {
+                contains = true;
             }
         }
-        if (!contains){
+        if (!contains) {
             this._elements.push(element);
         }
-        
     }
 
     addArc(arc: Arc): void {
-        let contains:boolean;
-        contains=false;
-        for (const item of this._arcs){
-            if (item.source===arc.source&&item.target===arc.target){
-                contains=true;
+        let contains: boolean;
+        contains = false;
+        for (const item of this._arcs) {
+            if (item.source === arc.source && item.target === arc.target) {
+                contains = true;
             }
         }
-        if (!contains){
+        if (!contains) {
             this._arcs.push(arc);
         }
-        
     }
 
     isEmpty(): boolean {
         return this.arcs.length + this.elements.length === 0;
     }
-
 }
 
 export type Arc = {
