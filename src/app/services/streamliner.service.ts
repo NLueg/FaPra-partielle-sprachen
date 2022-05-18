a32e63c26def2814b20e6ceb2a49f04d7efb0693
Nico Lueg <nicolueg@yahoo.de>
2022-05-18 16:19:19 +0200

    PAR-10: fixes linting + fixes falsy workflow

--- src/app/services/streamliner.service.ts
+++ src/app/services/streamliner.service.ts
@@ -8,22 +8,18 @@ export class StreamlinerService {
     constructor(private toastr: ToastrService) {}
 
     public cleanupContent(fileContent: string): string {
-        let fileContentWithoutBlanks = this.removeBlankLines(fileContent);
-        let warningHeader = "File was changed during upload";
+        const fileContentWithoutBlanks = this.removeBlankLines(fileContent);
+        const warningHeader = 'File was changed during upload';
         if (fileContentWithoutBlanks !== fileContent) {
-            this.toastr.warning(
-                `Blank lines removed`,
-                warningHeader
-            );
+            this.toastr.warning(`Blank lines removed`, warningHeader);
         }
-        let fileContentWithoutDuplicates = this.removeDuplicateLines(fileContentWithoutBlanks);
+        const fileContentWithoutDuplicates = this.removeDuplicateLines(
+            fileContentWithoutBlanks
+        );
         if (fileContentWithoutDuplicates !== fileContentWithoutBlanks) {
-            this.toastr.warning(
-                `Duplicate lines removed`,
-                warningHeader
-            );
+            this.toastr.warning(`Duplicate lines removed`, warningHeader);
         }
-       /* let fileContentWithoutInvalidArcs = this.removeInvalidArcs(fileContentWithoutDuplicates);
+        /* let fileContentWithoutInvalidArcs = this.removeInvalidArcs(fileContentWithoutDuplicates);
         if (fileContentWithoutInvalidArcs !== fileContentWithoutDuplicates) {
             this.toastr.warning(
                 `Invalid Arcs Removed`,
@@ -34,27 +30,31 @@ export class StreamlinerService {
     }
 
     private removeBlankLines(fileContent: string): string {
-        return fileContent.replace(/^\s*\n/gm, "");
+        return fileContent.replace(/^\s*\n/gm, '');
     }
 
     private removeDuplicateLines(fileContent: string): string {
-        return fileContent.split("\n").filter((line, i, allLines) => {
-            return i === allLines.indexOf(line);
-        }).join("\n");
+        return fileContent
+            .split('\n')
+            .filter((line, i, allLines) => {
+                return i === allLines.indexOf(line);
+            })
+            .join('\n');
     }
 
     private removeInvalidArcs(fileContent: string): string {
-        let lineArray = fileContent.split("\n");
-        let beginningOfArcs = lineArray.indexOf(".arcs") + 1;
-        let firstLines = beginningOfArcs > -1 ? lineArray.slice(0, beginningOfArcs) : [];
-        let arcs = lineArray.slice(beginningOfArcs);
-        let validArcs = arcs.filter(this.isValidArc(firstLines));
-        return firstLines.join("\n") + "\n" + validArcs.join("\n");
+        const lineArray = fileContent.split('\n');
+        const beginningOfArcs = lineArray.indexOf('.arcs') + 1;
+        const firstLines =
+            beginningOfArcs > -1 ? lineArray.slice(0, beginningOfArcs) : [];
+        const arcs = lineArray.slice(beginningOfArcs);
+        const validArcs = arcs.filter(this.isValidArc(firstLines));
+        return firstLines.join('\n') + '\n' + validArcs.join('\n');
     }
 
     private isValidArc(transitions: string[]) {
-        return function(arc: string): boolean {
-            let arcElements = arc.split(" ");
+        return function (arc: string): boolean {
+            const arcElements = arc.split(' ');
             if (arcElements.length !== 2) {
                 return false;
             }
@@ -65,6 +65,6 @@ export class StreamlinerService {
                 return false;
             }
             return transitions.indexOf(arcElements[1]) !== -1;
-        }
+        };
     }
 }
