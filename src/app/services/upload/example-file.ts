65f11523ec28aff29cf2e47721993cf80ea8cfc0
elisabeth brandl <elisabeth_brandl@gmx.net>
2022-08-11 11:59:40 +0200

    ID's angepasst(gleiche IDs in den unterschiedlichen runs), sodass wir beim merge besser unsere Fehler sehen

--- src/app/services/upload/example-file.ts
+++ src/app/services/upload/example-file.ts
@@ -2,19 +2,19 @@ export const exampleContent1 = `.type run
 .events
 1 | Reise planen
 2 | Prüfen
-6 | Flug suchen
-7 | Flug buchen
-8 | Hotel suchen
-9 | Hotel buchen
-10 | Unterlagen speichern
+3 | Flug suchen
+4 | Flug buchen
+5 | Hotel suchen
+6 | Hotel buchen
+7 | Unterlagen speichern
 .arcs
 1 2
-2 6
-2 8
+2 3
+2 5
+3 4
+4 7
+5 6
 6 7
-7 10
-8 9
-9 10
 `;
 
 export const exampleContent2 = `.type run
@@ -24,20 +24,20 @@ export const exampleContent2 = `.type run
 3 | Änderung anfordern
 4 | Reise planen
 5 | Prüfen
-11 | Flug suchen
-12 | Flug buchen
-13 | Hotel suchen
-14 | Hotel buchen
-15 | Unterlagen speichern
+6 | Flug suchen
+7 | Flug buchen
+8 | Hotel suchen
+9 | Hotel buchen
+10 | Unterlagen speichern
 .arcs
 1 2
 2 3
 3 4
 4 5
-5 11
-5 13
-11 12
-12 15
-13 14
-14 15
+5 6
+5 8
+6 7
+7 10
+8 9
+9 10
 `;
